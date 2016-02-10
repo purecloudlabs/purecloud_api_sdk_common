de6159af23d64fc973529ad53968db7c568cc96e
Kevin Glinski <kevin.glinski@inin.com>
2016-02-10 13:06:40 -0500

    fixing require path

--- print_version.js
+++ print_version.js
@@ -1,4 +1,4 @@
-var pclibSwaggerVersion = require('PureCloudApiLibraries').swaggerVersioning();
+var pclibSwaggerVersion = require('./lib/app.js').swaggerVersioning();
 
 var version = pclibSwaggerVersion.getVersionString("version.json");
 console.log(version);
