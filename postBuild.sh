5425cacc449e916f0b1f56021b594a6adda2256e
Kevin Glinski <kevin.glinski@inin.com>
2016-02-11 14:47:20 -0500

    removing tag from post build

--- postBuild.sh
+++ postBuild.sh
@@ -2,7 +2,5 @@
 
 if [ -f $WORKSPACE/repo/newVersion.md ]; then
     git add .
-    git commit -m 'updating from build, version $SDK_VERSION'
-    git tag -l $SDK_VERSION
-    git tag -a -m 'release from build server' $SDK_VERSION
+    git commit -m 'updating from build, version'
 fi
