1cdb17336ada3607c1fcd12764186f79ea09f7ad
Kevin Glinski <kevin.glinski@inin.com>
2016-03-03 11:09:07 -0500

    adding download and sanitize script

---
+++ downloadAndSanitizeSwagger.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+const SWAGGER_FILE_PATH_INDEX = 2;
+const ENVIRONMENT_INDEX = 3;
+
+if (process.argv.length <= VERSION_FILE_PATH_INDEX){
+    console.log("USAGE: node updateSwaggerAndVersionFiles <oldswaggerfilepath> <versionfilepath> <environment (default mypurecloud.com)>");
+    process.exit(1);
+}
+
+var pclib = require('./lib/app.js');
+var pcSwaggerGen = pclib.swaggerGen();
+
+var environment = "mypurecloud.com";
+if (process.argv.length <= ENVIRONMENT_INDEX){
+    environment = process.argv[ENVIRONMENT_INDEX];
+}
+
+pcSwaggerGen.downloadSwaggerFile(environment, function(swagger){
+    pcSwaggerGen.sanitizeSwagger(swagger, function(cleanSwagger){
+        fs.writeFileSync(process.argv[SWAGGER_FILE_PATH_INDEX], JSON.stringify(newSwagger));
+    } );
+});
